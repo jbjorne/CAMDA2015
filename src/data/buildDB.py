e5979e17b9d2dd6afeb0cb40cb98f8ca76aa8457
jari <jari.bjorne@utu.fi>
2014-04-15 16:33:41 +0300

    Automating

--- src/data/buildDB.py
+++ src/data/buildDB.py
@@ -1,19 +1,28 @@
 import csv, sqlite3
+import re
 import os, sys
 
 dataPath = os.path.expanduser("~/data/CAMDA2014/ICGC/Breast_Invasive_Carcinoma-TCGA-US/")
 
+def defineTable(name, header, columnTypes, defaultType=None):
+    columns = []
+    for i in range(len(header)):
+        if i in columnTypes:
+            columns += columnTypes[i]
+        elif header[i] in columnTypes:
+            columns += header[i] + " " + columnTypes[header[i]]
+        else:
+            columns += header[i] + " text"
+    return "create table " + name + "(" + ", ".join(columns) + ")"
+
 con = sqlite3.connect(":memory:")
-cur = con.cursor()
-cur.execute("CREATE TABLE t (col1, col2);")
+data = csv.reader(open(dataPath + "clinical.BRCA-US.tsv"), delimiter='\t')
+header = data.next()
 
-with open(dataPath + 'clinical.BRCA-US.tsv','rb') as fin: # `with` statement available in 2.5+
-    # csv.DictReader uses first line in file for column headings by default
-    dr = csv.DictReader(fin) # comma is default delimiter
-    print dr[0]
-    sys.exit()
-    to_db = [(i['col1'], i['col2']) for i in dr]
+defineTable("clinical", header, {re.compile("*age*"):"int"})
 
-cur.executemany("INSERT INTO t (col1, col2) VALUES (?, ?);", to_db)
-con.commit()
-
+for x in data:
+    print x
+    break
+#con.execute("create table person(firstname, lastname)")
+#con.executemany("insert into person(firstname, lastname) values (?, ?)", persons)
