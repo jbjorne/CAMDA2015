7c82315145f01335fd5a260ad3ecdd5036844463
jari <jari.bjorne@utu.fi>
2014-05-10 22:56:19 +0300

    Added the required imports.

--- src/learn/skext/gridSearch.py
+++ src/learn/skext/gridSearch.py
@@ -1,7 +1,121 @@
 import numpy as np
-from sklearn.grid_search import BaseSearchCV, _CVScoreTuple
+from collections import Sized
+import numbers
+import time
+from sklearn.grid_search import BaseSearchCV, _CVScoreTuple, is_classifier
 from sklearn.utils.validation import _num_samples, check_arrays
 from sklearn.metrics.scorer import _deprecate_loss_and_score_funcs
+from sklearn.cross_validation import check_cv
+from sklearn.base import clone
+from sklearn.externals.joblib import Parallel, delayed, logger
+from sklearn.utils import safe_mask
+
+def fit_grid_point_extended(X, y, base_estimator, parameters, train, test, scorer,
+                   verbose, loss_func=None, **fit_params):
+    """Run fit on one set of parameters.
+
+Parameters
+----------
+X : array-like, sparse matrix or list
+Input data.
+
+y : array-like or None
+Targets for input data.
+
+base_estimator : estimator object
+This estimator will be cloned and then fitted.
+
+parameters : dict
+Parameters to be set on base_estimator clone for this grid point.
+
+train : ndarray, dtype int or bool
+Boolean mask or indices for training set.
+
+test : ndarray, dtype int or bool
+Boolean mask or indices for test set.
+
+scorer : callable or None.
+If provided must be a scorer callable object / function with signature
+``scorer(estimator, X, y)``.
+
+verbose : int
+Verbosity level.
+
+**fit_params : kwargs
+Additional parameter passed to the fit function of the estimator.
+
+
+Returns
+-------
+score : float
+Score of this parameter setting on given training / test split.
+
+parameters : dict
+The parameters that have been evaluated.
+
+n_samples_test : int
+Number of test samples in this split.
+"""
+    if verbose > 1:
+        start_time = time.time()
+        msg = '%s' % (', '.join('%s=%s' % (k, v)
+                      for k, v in parameters.items()))
+        print("[GridSearchCV] %s %s" % (msg, (64 - len(msg)) * '.'))
+
+    # update parameters of the classifier after a copy of its base structure
+    clf = clone(base_estimator)
+    clf.set_params(**parameters)
+
+    if hasattr(base_estimator, 'kernel') and callable(base_estimator.kernel):
+        # cannot compute the kernel values with custom function
+        raise ValueError("Cannot use a custom kernel function. "
+                         "Precompute the kernel matrix instead.")
+
+    if not hasattr(X, "shape"):
+        if getattr(base_estimator, "_pairwise", False):
+            raise ValueError("Precomputed kernels or affinity matrices have "
+                             "to be passed as arrays or sparse matrices.")
+        X_train = [X[idx] for idx in train]
+        X_test = [X[idx] for idx in test]
+    else:
+        if getattr(base_estimator, "_pairwise", False):
+            # X is a precomputed square kernel matrix
+            if X.shape[0] != X.shape[1]:
+                raise ValueError("X should be a square kernel matrix")
+            X_train = X[np.ix_(train, train)]
+            X_test = X[np.ix_(test, train)]
+        else:
+            X_train = X[safe_mask(X, train)]
+            X_test = X[safe_mask(X, test)]
+
+    if y is not None:
+        y_test = y[safe_mask(y, test)]
+        y_train = y[safe_mask(y, train)]
+        clf.fit(X_train, y_train, **fit_params)
+
+        if scorer is not None:
+            this_score = scorer(clf, X_test, y_test)
+        else:
+            this_score = clf.score(X_test, y_test)
+    else:
+        clf.fit(X_train, **fit_params)
+        if scorer is not None:
+            this_score = scorer(clf, X_test)
+        else:
+            this_score = clf.score(X_test)
+
+    if not isinstance(this_score, numbers.Number):
+        raise ValueError("scoring must return a number, got %s (%s)"
+                         " instead." % (str(this_score), type(this_score)))
+
+    if verbose > 2:
+        msg += ", score=%f" % this_score
+    if verbose > 1:
+        end_msg = "%s -%s" % (msg,
+                              logger.short_format_time(time.time() -
+                                                       start_time))
+        print("[GridSearchCV] %s %s" % ((64 - len(end_msg)) * '.', end_msg))
+    return this_score, parameters, _num_samples(X_test)
 
 class ExtendedBaseSearchCV(BaseSearchCV):
     """Base class for hyper parameter search with cross-validation."""
@@ -40,7 +154,7 @@ class ExtendedBaseSearchCV(BaseSearchCV):
         out = Parallel(
             n_jobs=self.n_jobs, verbose=self.verbose,
             pre_dispatch=pre_dispatch)(
-                delayed(fit_grid_point)(
+                delayed(fit_grid_point_extended)(
                     X, y, base_estimator, parameters, train, test,
                     self.scorer_, self.verbose, **self.fit_params)
                 for parameters in parameter_iterable
