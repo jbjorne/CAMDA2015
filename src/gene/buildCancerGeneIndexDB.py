b999bcb450400fa3ebfd99ba986d2d3b6bab2f2f
jari <jari.bjorne@utu.fi>
2014-05-18 15:54:08 +0300

    Switched to cElementTree.

--- src/gene/buildCancerGeneIndexDB.py
+++ src/gene/buildCancerGeneIndexDB.py
@@ -3,34 +3,18 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 import settings
 import data.buildDB as DB
 import utils.download as download
-import lxml.etree as ET
+import xml.etree.cElementTree as ET
 import zipfile
 
-def fast_iter(context, func, *args, **kwargs):
-    """
-    From http://stackoverflow.com/questions/7171140/using-python-iterparse-for-large-xml-files
-    by user unutbu
-    
-    http://lxml.de/parsing.html#modifying-the-tree
-    Based on Liza Daly's fast_iter
-    http://www.ibm.com/developerworks/xml/library/x-hiperfparse/
-    See also http://effbot.org/zone/element-iterparse.htm
-    """
-    for event, elem in context:
-        func(elem, *args, **kwargs)
-        # It's safe to call clear() here because no descendants will be
-        # accessed
-        elem.clear()
-        # Also eliminate now-empty references from the root node to elem
-        for ancestor in elem.xpath('ancestor-or-self::*'):
-            while ancestor.getprevious() is not None:
-                del ancestor.getparent()[0]
-    del context
+def iterparse(xml, tag):
+    for event, elem in ET.iterparse(xml): # get end events
+        if elem.tag == tag:
+            yield elem
+            elem.clear()
 
-
-def process_element(elem):
-    print elem
-    print elem.xpath( 'description/text( )' )
+# def process_element(elem):
+#     print elem
+#     #print elem.xpath( 'description/text( )' )
 
 def buildDB(filename, downloadDir):
     if downloadDir == None:
@@ -41,8 +25,9 @@ def buildDB(filename, downloadDir):
     #sys.exit()
     
     item = os.path.basename(diseaseFile).split(".")[0] + ".xml"
-    context = ET.iterparse(zf.open(item), tag='GeneEntry' )
-    fast_iter(context,process_element)
+    for elem in iterparse(zf.open(item), tag='GeneEntry'):
+        print elem
+    zf.close()
 
 if __name__ == "__main__":
     import argparse
