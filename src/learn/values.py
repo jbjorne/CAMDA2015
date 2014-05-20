47bee25fcce3da60081a6c70ce3e7e3e54ba4730
jari <jari.bjorne@utu.fi>
2014-05-20 17:25:12 +0300

    More work on results processing.

--- src/learn/values.py
+++ src/learn/values.py
@@ -32,10 +32,13 @@ def makeProjectTable(projects):
         for experimentName in ["CANCER_OR_CONTROL", "REMISSION"]:
             if experimentName in project:
                 experiment = project[experimentName]
-                #if experiment["classifier"] == "ensemble.ExtraTreesClassifier":
-                row[(experimentName, experiment["classifier"])] = experiment["auc-hidden"]
-                row[(experimentName, 1)] = experiment["1"]
-                row[(experimentName, -1)] = experiment["-1"]
+                for classifierName in ["LinearSVC", "ExtraTreesClassifier"]:
+                    if classifierName in experiment:
+                        classifier = experiment[classifierName]
+                        #if experiment["classifier"] == "ensemble.ExtraTreesClassifier":
+                        row[(experimentName, classifierName)] = classifier["auc-hidden"]
+                        row[(experimentName, 1)] = classifier["1"]
+                        row[(experimentName, -1)] = classifier["-1"]
         rows.append(row)
     columns = ["project",
                ("CANCER_OR_CONTROL", 1),
@@ -46,14 +49,14 @@ def makeProjectTable(projects):
                ("REMISSION", -1),
                ("REMISSION", "ExtraTreesClassifier"),
                ("REMISSION", "LinearSVC")]
-    columnNames = {("CANCER_OR_CONTROL", "ExtraTreesClassifier"):"AUC (ETC)",
-                   ("CANCER_OR_CONTROL", "LinearSVC"):"AUC (SVM)",
-                   ("CANCER_OR_CONTROL", 1):"pos",
-                   ("CANCER_OR_CONTROL", -1):"neg",
-                   ("REMISSION", "ExtraTreesClassifier"):"AUC (ETC)",
-                   ("REMISSION", "LinearSVC"):"AUC (SVM)",
-                   ("REMISSION", 1):"pos",
-                   ("REMISSION", -1):"neg"}
+    columnNames = {("CANCER_OR_CONTROL", "ExtraTreesClassifier"):"$AUC_E$",
+                   ("CANCER_OR_CONTROL", "LinearSVC"):"$AUC_S$",
+                   ("CANCER_OR_CONTROL", 1):"cancer",
+                   ("CANCER_OR_CONTROL", -1):"normal",
+                   ("REMISSION", "ExtraTreesClassifier"):"$AUC_E$",
+                   ("REMISSION", "LinearSVC"):"$AUC_S$",
+                   ("REMISSION", 1):"remission",
+                   ("REMISSION", -1):"progression"}
     header = "project & \multicolumn{2}{c}{Multi-column}"
     return makeTableLatex(columns, rows, columnNames)
 
@@ -67,32 +70,39 @@ def getProjects(dirname, projectFilter):
     projects = {}
     print "Reading results from", dirname
     filenames = os.listdir(dirname)
-    for index, filename in enumerate(filenames):
-        filePath = os.path.join(dirname, filename)
-        found = True
-        if projectFilter != None:
-            found = False
-            for projectName in projectFilter:
-                if projectName in filename:
-                    found = True
-                    break
-        if found and os.path.isfile(filePath) and filePath.endswith(".json"):
-            print "Processing", filename, str(index+1) + "/" + str(len(filenames))
-            meta = result.getMeta(filePath)
-            projectName = meta["template"]["project"]
-            if projectName not in projects:
-                projects[projectName] = {}
-            project = projects[projectName]
-            experimentName = meta["experiment"]["name"]
-            if experimentName not in project:
-                project[experimentName] = {}
-            experiment = project[experimentName]
-            experiment["classifier"] = meta["results"]["best"]["classifier"]
-            experiment["classifier-details"] = meta["results"]["hidden"]["classifier"]
-            experiment["auc-hidden"] = meta["results"]["hidden"]["roc_auc"]
-            experiment["auc-train"] = meta["results"]["best"]["mean"]
-            experiment["std-train"] = meta["results"]["best"]["std"]
-            experiment.update(countExamples(meta))
+    index = 0
+    for dirpath, dirnames, filenames in os.walk(dirname):
+        for filename in filenames:
+            index += 1
+            filePath = os.path.join(dirpath, filename)
+            found = True
+            if projectFilter != None:
+                found = False
+                for projectName in projectFilter:
+                    if projectName in filename:
+                        found = True
+                        break
+            if found and os.path.isfile(filePath) and filePath.endswith(".json"):
+                print "Processing", filename, str(index+1) #+ "/" + str(len(filenames))
+                meta = result.getMeta(filePath)
+                projectName = meta["template"]["project"]
+                if projectName not in projects:
+                    projects[projectName] = {}
+                project = projects[projectName]
+                experimentName = meta["experiment"]["name"]
+                if experimentName not in project:
+                    project[experimentName] = {}
+                experiment = project[experimentName]
+                classifierName = meta["results"]["best"]["classifier"]
+                if classifierName not in experiment:
+                    experiment[classifierName] = {}
+                classifier = experiment[classifierName]
+                #experiment["classifier"] = meta["results"]["best"]["classifier"]
+                classifier["classifier-details"] = meta["results"]["hidden"]["classifier"]
+                classifier["auc-hidden"] = meta["results"]["hidden"]["roc_auc"]
+                classifier["auc-train"] = meta["results"]["best"]["mean"]
+                classifier["std-train"] = meta["results"]["best"]["std"]
+                classifier.update(countExamples(meta))
     return projects
 
 def process(dirname, projectFilter):
