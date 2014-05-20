636a4f79a4565a7e4f1b8a5468140496cc123b58
jari <jari.bjorne@utu.fi>
2014-05-20 16:36:00 +0300

    More work on extracting results.

--- src/learn/values.py
+++ src/learn/values.py
@@ -4,16 +4,24 @@ import settings
 import data.result as result
 import copy
 
+def formatValue(value):
+    if isinstance(value, basestring):
+        return value
+    elif isinstance(value, int):
+        return str(value)
+    else:
+        return str("%0.2f" % value)
+
 def makeTableLatex(columns, rows, columnNames = {}):
     columnNames = copy.copy(columnNames)
     for column in columns:
         if column not in columnNames:
             columnNames[column] = column
-    header = " & ".join([columnNames[column] for column in columns]) + " \\\n"
+    header = " & ".join([columnNames[column] for column in columns]) + " \\\\\n"
     body = ""
     for row in rows:
-        print row
-        body += " & ".join([row.get(column, "-") for column in columns]) + " \\\n"
+        #print row, columns
+        body += " & ".join([formatValue(row.get(column, "-")) for column in columns]) + " \\\\\n"
     return header + body
     
 def makeProjectTable(projects):
@@ -26,13 +34,34 @@ def makeProjectTable(projects):
                 experiment = project[experimentName]
                 #if experiment["classifier"] == "ensemble.ExtraTreesClassifier":
                 row[(experimentName, experiment["classifier"])] = experiment["auc-hidden"]
+                row[(experimentName, 1)] = experiment["1"]
+                row[(experimentName, -1)] = experiment["-1"]
         rows.append(row)
-    columns = ["project", 
-               ("CANCER_OR_CONTROL", "ensemble.ExtraTreesClassifier"),
-               ("REMISSION", "ensemble.ExtraTreesClassifier")]
-    columnNames = {("CANCER_OR_CONTROL", "ensemble.ExtraTreesClassifier"):"AUC",
-                   ("REMISSION", "ensemble.ExtraTreesClassifier"):"AUC"}
+    columns = ["project",
+               ("CANCER_OR_CONTROL", 1),
+               ("CANCER_OR_CONTROL", -1),
+               ("CANCER_OR_CONTROL", "ExtraTreesClassifier"),
+               ("CANCER_OR_CONTROL", "LinearSVC"),
+               ("REMISSION", 1),
+               ("REMISSION", -1),
+               ("REMISSION", "ExtraTreesClassifier"),
+               ("REMISSION", "LinearSVC")]
+    columnNames = {("CANCER_OR_CONTROL", "ExtraTreesClassifier"):"AUC (ETC)",
+                   ("CANCER_OR_CONTROL", "LinearSVC"):"AUC (SVM)",
+                   ("CANCER_OR_CONTROL", 1):"pos",
+                   ("CANCER_OR_CONTROL", -1):"neg",
+                   ("REMISSION", "ExtraTreesClassifier"):"AUC (ETC)",
+                   ("REMISSION", "LinearSVC"):"AUC (SVM)",
+                   ("REMISSION", 1):"pos",
+                   ("REMISSION", -1):"neg"}
+    header = "project & \multicolumn{2}{c}{Multi-column}"
     return makeTableLatex(columns, rows, columnNames)
+
+def countExamples(meta):
+    counts = {"1":0, "-1":0}
+    for example in meta["examples"]:
+        counts[example["label"]] += 1
+    return counts
         
 def getProjects(dirname, projectFilter):
     projects = {}
@@ -63,6 +92,7 @@ def getProjects(dirname, projectFilter):
             experiment["auc-hidden"] = meta["results"]["hidden"]["roc_auc"]
             experiment["auc-train"] = meta["results"]["best"]["mean"]
             experiment["std-train"] = meta["results"]["best"]["std"]
+            experiment.update(countExamples(meta))
     return projects
 
 def process(dirname, projectFilter):
