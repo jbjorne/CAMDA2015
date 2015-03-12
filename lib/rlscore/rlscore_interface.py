eca58f9ba6bfb88e9605501ef02de117706dc448
jari <jari.bjorne@utu.fi>
2015-03-12 23:44:54 +0200

    Added the 'get_params' and 'set_params' methods required by a scikit-learn estimator.

--- lib/rlscore/rlscore_interface.py
+++ lib/rlscore/rlscore_interface.py
@@ -3,14 +3,21 @@ from rlscore.learner import rls
 class RLSInterface(object):
 
     def __init__(self, alpha = 1.0):
-        self.regparam = alpha
+        self.alpha = alpha
 
     def fit(self, X, y):
         keyw = {"train_features":X, "train_labels":y}
         learner = rls.RLS(**keyw)
-        learner.solve(self.regparam)
+        learner.solve(self.alpha)
         self.model = learner.getModel()
 
     def predict(self, X):
         p = self.model.predict(X)
         return p
+    
+    def get_params(self, deep=True):
+        return {"alpha": self.alpha}
+
+    def set_params(self, **parameters):
+        for parameter, value in parameters.items():
+            setattr(self, parameter, value)
