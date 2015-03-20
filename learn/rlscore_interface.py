c92b3d70a1c36cee5842035ec41a084d4a441975
ajairo <ajairo@utu.fi>
2015-03-20 16:18:09 +0200

    Updated greedyRLS to the interface.

--- learn/rlscore_interface.py
+++ learn/rlscore_interface.py
@@ -2,17 +2,25 @@ import sys, os
 basePath = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 sys.path.append(os.path.join(basePath, "rlscore"))
 from rlscore.learner import rls
+from rlscore.learner import greedy_rls
 
 class RLScore(object):
 
-    def __init__(self, alpha = 1.0):
+    def __init__(self, alpha = 1.0, subsetsize=None):
         self.alpha = alpha
+        self.subsetsize = subsetsize
 
     def fit(self, X, y):
         keyw = {"train_features":X, "train_labels":y}
-        learner = rls.RLS(**keyw)
+        if self.fcount == None:
+            learner = rls.RLS(**keyw)
+        else:
+            keyw["subsetsize"] = self.subsetsize
+            learner = greedy_rls.GreedyRLS(**keyw)
         learner.solve(self.alpha)
         self.model = learner.getModel()
+        if self.subsetsize != None:
+            self.selected = learner.selected
     
     def decision_function(self, X):
         p = self.model.predict(X)
@@ -33,3 +41,5 @@ class RLScore(object):
         for parameter, value in parameters.items():
             setattr(self, parameter, value)
         return self
+
+
