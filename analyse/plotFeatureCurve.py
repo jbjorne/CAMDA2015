a5654d4d5cba27129accfa00bd1814d7c0bb459f
jari <jari.bjorne@utu.fi>
2015-03-31 19:10:38 +0300

    Plot the results for multiple classifiers.

--- analyse/plotFeatureCurve.py
+++ analyse/plotFeatureCurve.py
@@ -3,10 +3,12 @@ import json
 import matplotlib.pyplot as plt
 from collections import OrderedDict
 
-def loadData(sourcePath):
+def loadData(sourcePath, filter=None):
     print "Loading results from", sourcePath
     projects = {}
     for filename in os.listdir(sourcePath):
+        if filter and filter not in filename:
+            continue
         if filename.endswith("json"):
             print "Reading result file", filename
             f = open(os.path.join(sourcePath, filename), "rt")
@@ -14,50 +16,67 @@ def loadData(sourcePath):
             f.close()
             
             projectName = meta["template"]["project"]
-            if projectName not in projects:
-                projects[projectName] = {}
+            classifierName = meta["results"]["hidden"]["classifier"]
+            if classifierName not in projects:
+                projects[classifierName] = {}
+            if projectName not in projects[classifierName]:
+                project = {}
+                projects[classifierName][projectName] = project
+            else:
+                project = projects[classifierName][projectName]
+            
             pointIndex = meta["curve"]["count"]
             pointValue = meta["results"]["hidden"]["score"]
-            assert pointIndex not in projects[projectName]
-            projects[projectName][pointIndex] = pointValue
-    for projectName in projects:
-        project = projects[projectName]
-        vector = []
-        for i in range(max(project.keys())):
-            if i in project:
-                vector.append(project[i])
-            else:
-                vector.append(None)
-        projects[projectName] = vector
+            assert pointIndex not in project
+            project[pointIndex] = pointValue
+    
+    for classifierName in projects:
+        for projectName in projects[classifierName]:
+            project = projects[classifierName][projectName]
+            vector = []
+            for i in range(max(project.keys())):
+                if i in project:
+                    vector.append(project[i])
+                else:
+                    vector.append(None)
+            projects[classifierName][projectName] = vector
     return projects
 
-def process(inPath):
-    projects = loadData(inPath)
-    colors = ["black", "blue", "red"]
-    styles = ["--", "-", ":"]
-    minY = 0.0
-    maxY = 1.0
-    minX = 0.0
-    maxX = 1.0
-    for project, color, style in zip(sorted(projects.keys()), colors, styles):
-        x = range(2, len(projects[project]) + 2)
-        points = projects[project]
-        plt.plot(x, points, linestyle=style, color=color, label=project)
-        minY = min([value for value in points if value != None])
-        maxY = max([value for value in points if value != None])
-        minX = min(x)
-        maxX = max(x)
-    plt.xlabel("#features")
-    plt.ylabel("AUC")
-    plt.legend(loc=4)
-    plt.ylim([minY - 0.1, maxY + 0.1])
-    plt.xlim([minX, maxX])
-    plt.show()
+def process(inPath, filter=None):
+    projects = loadData(inPath, filter)
+    outPath = os.path.join(os.path.dirname(inPath), "figures")
+    if not os.path.exists(outPath):
+        os.makedirs(outPath)
+    
+    colors = {"HNSC-US":"black", "KIRC-US":"blue", "LUAD-US":"red"}
+    styles = {"HNSC-US":"--", "KIRC-US":"-", "LUAD-US":":"}
+         
+    for classifierName in projects:
+        minY = 1.0
+        maxY = 0.0
+        minX = 1.0
+        maxX = 0.0
+        for projectName in projects[classifierName]:
+            points = projects[classifierName][projectName]
+            x = range(2, len(points) + 2)
+            plt.plot(x, points, linestyle=styles[projectName], color=colors[projectName], label=projectName)
+            plt.xlabel("#features")
+            plt.ylabel("AUC")
+            plt.legend(loc=4)
+            minY = min([minY] + [value for value in points if value != None])
+            maxY = max([maxY] + [value for value in points if value != None])
+            minX = min([minX] + x)
+            maxX = max([maxX] + x)
+            plt.ylim([minY - 0.01, maxY + 0.01])
+            plt.xlim([minX, maxX])
+        plt.savefig(os.path.join(outPath, classifierName + ".png"))
+        plt.cla()
 
 if __name__ == "__main__":
     import argparse
     parser = argparse.ArgumentParser(description='Learning with examples')
     parser.add_argument('-i','--input', help='Feature curve JSON file', default=None)
+    parser.add_argument('-f','--filter', help='', default=None)
     options = parser.parse_args()
     
-    process(options.input)
+    process(options.input, options.filter)
