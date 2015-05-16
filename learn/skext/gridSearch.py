046020731e637da830b44140fd81851252266881
jari <jari.bjorne@utu.fi>
2015-05-16 19:31:11 +0300

    Updating the extended grid seach for scikit-learn 0.16.1.

--- learn/skext/gridSearch.py
+++ learn/skext/gridSearch.py
@@ -1,159 +1,34 @@
-import numpy as np
+from sklearn.grid_search import GridSearchCV, ParameterGrid, _CVScoreTuple
+from sklearn.metrics.scorer import check_scoring
+from sklearn.utils.validation import _num_samples, indexable
+from sklearn.cross_validation import _check_cv as check_cv
+from sklearn.base import is_classifier, clone
+from sklearn.externals.joblib import Parallel, delayed
+from sklearn.cross_validation import _fit_and_score
 from collections import Sized
-import numbers
-import time
-import warnings
-from sklearn.grid_search import ParameterGrid, BaseSearchCV, _CVScoreTuple, is_classifier, _check_param_grid
-from sklearn.utils.validation import _num_samples, check_arrays
-from sklearn.metrics.scorer import _deprecate_loss_and_score_funcs
-from sklearn.cross_validation import check_cv
-from sklearn.base import clone
-from sklearn.externals.joblib import Parallel, delayed, logger
-from sklearn.utils import safe_mask
+import numpy as np
 
-def fit_grid_point_extended(X, y, base_estimator, parameters, train, test, scorer,
-                   verbose, loss_func=None, extraOut="auto", **fit_params):
-    """Run fit on one set of parameters.
+class ExtendedGridSearchCV(GridSearchCV):
 
-    Parameters
-    ----------
-    X : array-like, sparse matrix or list
-    Input data.
-    
-    y : array-like or None
-    Targets for input data.
-    
-    base_estimator : estimator object
-    This estimator will be cloned and then fitted.
-    
-    parameters : dict
-    Parameters to be set on base_estimator clone for this grid point.
-    
-    train : ndarray, dtype int or bool
-    Boolean mask or indices for training set.
-    
-    test : ndarray, dtype int or bool
-    Boolean mask or indices for test set.
-    
-    scorer : callable or None.
-    If provided must be a scorer callable object / function with signature
-    ``scorer(estimator, X, y)``.
-    
-    verbose : int
-    Verbosity level.
-    
-    **fit_params : kwargs
-    Additional parameter passed to the fit function of the estimator.
-    
-    
-    Returns
-    -------
-    score : float
-    Score of this parameter setting on given training / test split.
-    
-    parameters : dict
-    The parameters that have been evaluated.
-    
-    n_samples_test : int
-    Number of test samples in this split.
-    """
-    if verbose > 1:
-        start_time = time.time()
-        msg = '%s' % (', '.join('%s=%s' % (k, v)
-                      for k, v in parameters.items()))
-        print("[GridSearchCV] %s %s" % (msg, (64 - len(msg)) * '.'))
+    def __init__(self, *args, **kwargs):
+        super(GridSearchCV, self).__init__(*args, **kwargs)
 
-    # update parameters of the classifier after a copy of its base structure
-    clf = clone(base_estimator)
-    clf.set_params(**parameters)
-
-    if hasattr(base_estimator, 'kernel') and callable(base_estimator.kernel):
-        # cannot compute the kernel values with custom function
-        raise ValueError("Cannot use a custom kernel function. "
-                         "Precompute the kernel matrix instead.")
-
-    if not hasattr(X, "shape"):
-        if getattr(base_estimator, "_pairwise", False):
-            raise ValueError("Precomputed kernels or affinity matrices have "
-                             "to be passed as arrays or sparse matrices.")
-        X_train = [X[idx] for idx in train]
-        X_test = [X[idx] for idx in test]
-    else:
-        if getattr(base_estimator, "_pairwise", False):
-            # X is a precomputed square kernel matrix
-            if X.shape[0] != X.shape[1]:
-                raise ValueError("X should be a square kernel matrix")
-            X_train = X[np.ix_(train, train)]
-            X_test = X[np.ix_(test, train)]
-        else:
-            X_train = X[safe_mask(X, train)]
-            X_test = X[safe_mask(X, test)]
-
-    if y is not None:
-        y_test = y[safe_mask(y, test)]
-        y_train = y[safe_mask(y, train)]
-        clf.fit(X_train, y_train, **fit_params)
-
-        if scorer is not None:
-            this_score = scorer(clf, X_test, y_test)
-        else:
-            this_score = clf.score(X_test, y_test)
-    else:
-        clf.fit(X_train, **fit_params)
-        if scorer is not None:
-            this_score = scorer(clf, X_test)
-        else:
-            this_score = clf.score(X_test)
-
-    if not isinstance(this_score, numbers.Number):
-        raise ValueError("scoring must return a number, got %s (%s)"
-                         " instead." % (str(this_score), type(this_score)))
-
-    if verbose > 2:
-        msg += ", score=%f" % this_score
-    if verbose > 1:
-        end_msg = "%s -%s" % (msg,
-                              logger.short_format_time(time.time() -
-                                                       start_time))
-        print("[GridSearchCV] %s %s" % ((64 - len(end_msg)) * '.', end_msg))
-    extraRVs = {}
-    if extraOut != None:
-        if "estimator" in extraOut:
-            extraRVs["estimator"] = clf
-        if extraOut == "auto" or "predictions" in extraOut:
-            predictions = clf.predict(X)
-            predictionIndex = 0
-            predictionByIndex = {}
-            for exampleIndex in safe_mask(X, test):
-                predictionByIndex[exampleIndex] = predictions[predictionIndex]
-                predictionIndex += 1
-            extraRVs["predictions"] = predictionByIndex
-        if (extraOut == "auto" or "importances" in extraOut) and hasattr(clf, "feature_importances_"):
-            extraRVs["importances"] = clf.feature_importances_
-    rvs = [this_score, parameters, _num_samples(X_test), extraRVs]
-    return rvs
-
-class ExtendedBaseSearchCV(BaseSearchCV):
-    """Base class for hyper parameter search with cross-validation."""
-
-    def _fit(self, X, y, parameter_iterable):
-        """Actual fitting, performing the search over parameters."""
-
+    def fit(self, X, y=None):
+        return self._fit(X, y, ParameterGrid(self.param_grid))
+    
+    def _extendedFit(self, X, y, parameter_iterable):
         estimator = self.estimator
         cv = self.cv
+        self.scorer_ = check_scoring(self.estimator, scoring=self.scoring)
 
         n_samples = _num_samples(X)
-        X, y = check_arrays(X, y, allow_lists=True, sparse_format='csr')
-
-        self.scorer_ = _deprecate_loss_and_score_funcs(
-            self.loss_func, self.score_func, self.scoring)
+        X, y = indexable(X, y)
 
         if y is not None:
             if len(y) != n_samples:
                 raise ValueError('Target variable (y) has a different number '
                                  'of samples (%i) than data (X: %i samples)'
                                  % (len(y), n_samples))
-            y = np.asarray(y)
         cv = check_cv(cv, X, y, classifier=is_classifier(estimator))
 
         if self.verbose > 0:
@@ -169,37 +44,28 @@ class ExtendedBaseSearchCV(BaseSearchCV):
 
         out = Parallel(
             n_jobs=self.n_jobs, verbose=self.verbose,
-            pre_dispatch=pre_dispatch)(
-                delayed(fit_grid_point_extended)(
-                    X, y, base_estimator, parameters, train, test,
-                    self.scorer_, self.verbose, **self.fit_params)
+            pre_dispatch=pre_dispatch
+        )(
+            delayed(_fit_and_score)(clone(base_estimator), X, y, self.scorer_,
+                                    train, test, self.verbose, parameters,
+                                    self.fit_params, return_parameters=True,
+                                    error_score=self.error_score)
                 for parameters in parameter_iterable
                 for train, test in cv)
-        
-#         out = []
-#         for parameters in parameter_iterable:
-#             fold = 1
-#             for train, test in cv:
-#                 print "Processing fold", fold, self.fit_params
-#                 out.append(fit_grid_point_extended(X, y, base_estimator, parameters, train, test, self.scorer_, self.verbose, **self.fit_params))
-#                 fold += 1
 
         # Out is a list of triplet: score, estimator, n_test_samples
         n_fits = len(out)
         n_folds = len(cv)
 
         scores = list()
-        grid_extras = list()
         grid_scores = list()
         for grid_start in range(0, n_fits, n_folds):
             n_test_samples = 0
             score = 0
             all_scores = []
-            all_extras = list()
-            for this_score, parameters, this_n_test_samples, extra in \
+            for this_score, this_n_test_samples, _, parameters in \
                     out[grid_start:grid_start + n_folds]:
                 all_scores.append(this_score)
-                all_extras.append(extra)
                 if self.iid:
                     this_score *= this_n_test_samples
                     n_test_samples += this_n_test_samples
@@ -214,10 +80,8 @@ class ExtendedBaseSearchCV(BaseSearchCV):
                 parameters,
                 score,
                 np.array(all_scores)))
-            grid_extras.append(all_extras)
         # Store the computed scores
         self.grid_scores_ = grid_scores
-        self.extras_ = grid_extras
 
         # Find the best parameters by comparing on the mean validation score:
         # note that `sorted` is deterministic in the way it breaks ties
@@ -237,21 +101,3 @@ class ExtendedBaseSearchCV(BaseSearchCV):
                 best_estimator.fit(X, **self.fit_params)
             self.best_estimator_ = best_estimator
         return self
-
-class ExtendedGridSearchCV(ExtendedBaseSearchCV):
-
-    def __init__(self, estimator, param_grid, scoring=None, loss_func=None,
-                 score_func=None, fit_params=None, n_jobs=1, iid=True,
-                 refit=True, cv=None, verbose=0, pre_dispatch='2*n_jobs'):
-        super(ExtendedGridSearchCV, self).__init__(
-            estimator, scoring, loss_func, score_func, fit_params, n_jobs, iid,
-            refit, cv, verbose, pre_dispatch)
-        self.param_grid = param_grid
-        _check_param_grid(param_grid)
-
-    def fit(self, X, y=None, **params):
-        if params:
-            warnings.warn("Additional parameters to GridSearchCV are ignored!"
-                          " The params argument will be removed in 0.15.",
-                          DeprecationWarning)
-        return self._fit(X, y, ParameterGrid(self.param_grid))
