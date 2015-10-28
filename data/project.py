a5bb2d9d0bbb28094f197b50d3598628dd96827c
jari <jari.bjorne@utu.fi>
2015-10-28 18:05:53 +0200

    Example generation and writing works with the new system.

--- data/project.py
+++ data/project.py
@@ -18,14 +18,14 @@ def connect(con):
     return con
 
 class Experiment:
-    def getExamples(self):
+    def _queryExamples(self):
         query = "SELECT " + self.exampleFields + "\n"
         query += "FROM " + self.exampleTable + "\n"
         query += "WHERE "
         if self.projects != None:
             query += " project_code IN " + self.projects + " AND" + "\n"
         query += self.exampleWhere
-        return [x for x in self.getConnection().execute(query)]
+        return [dict(x) for x in self.getConnection().execute(query)]
     
     def getLabel(self, example):
         return 'remission' in example['disease_status_last_followup']
@@ -48,8 +48,7 @@ class Experiment:
         self.featureIds = {}
         self.classIds = {'True':1, 'False':-1}
         # General
-        self.name = None
-        self.project = None
+        self.projects = None
         
         self.exampleTable = "clinical"
         self.exampleFields = "icgc_donor_id,icgc_specimen_id,project_code,donor_vital_status,disease_status_last_followup,specimen_type,donor_interval_of_last_followup"
@@ -64,6 +63,7 @@ class Experiment:
         self.meta = []
         # Output files
         self.metaDataFileName = None
+        self.writer = None
     
     def generateOrNot(self, example, verbose=True):
         if not self.includeHiddenSet and example["hidden"] < self.hiddenCutoff:
@@ -87,15 +87,15 @@ class Experiment:
             return value
     
     def getFeatureId(self, featureName):
-        if featureName not in self._featureIds:
-            self._featureIds[featureName] = len(self._featureIds)
-        return self._featureIds[featureName]
+        if featureName not in self.featureIds:
+            self.featureIds[featureName] = len(self.featureIds)
+        return self.featureIds[featureName]
         
     def _buildFeatures(self, example):
         features = {}
-        for featureGroup in self.featureGroups:
-            groupIndex = self.featureGroups.index(featureGroup)
-            for row in featureGroup(con=self.getConnection(), example=example):
+        for groupIndex in range(len(self.featureGroups)):
+            featureGroup = self.featureGroups[groupIndex]
+            for row in self._queryFeatures(featureGroup, example): #featureGroup(con=self.getConnection(), example=example):
                 for key, value in itertools.izip(*[iter(row)] * 2): # iterate over each consecutive key,value columns pair
                     if not isinstance(key, basestring):
                         raise Exception("Non-string feature key '" + str(key) + "' in feature group " + str(groupIndex))
@@ -106,19 +106,22 @@ class Experiment:
             print "WARNING: example has no features"
         return features
     
+    def _queryFeatures(self, featureGroup, example):
+        return self.getConnection().execute(featureGroup, (example['icgc_specimen_id'], ) )
+    
     def _filterExample(self, example):
         if self.filter != None:
-            return len([x for x in self.getConnection().execute(self.filter, example['icgc_specimen_id'])]) == 0
+            return len([x for x in self.getConnection().execute(self.filter, (example['icgc_specimen_id'], ) )]) == 0
         return False
     
     def getExampleMeta(self, example, classId, features):
         return dict(example, label=str(classId), features=len(features))
     
-    def buildExamples(self, outputFileName=None, metaDataFileName=None):
-        print "Template:", self.name
-        self.examples = self.getExamples()
+    def buildExamples(self, metaDataFileName=None, exampleWriter=None):
+        print "Template:", self.__class__.__name__
+        self.examples = self._queryExamples()
         self.meta = []
-        numHidden = hidden.setHiddenValuesByFraction(self.examples, self.hidden)
+        numHidden = hidden.setHiddenValuesByFraction(self.examples, self.hiddenCutoff)
         numExamples = len(self.examples)
         print "Examples " +  str(numExamples) + ", hidden " + str(numHidden)
         count = 0
@@ -137,14 +140,13 @@ class Experiment:
             
             features = self._buildFeatures(example)
             self.meta.append(self.getExampleMeta(example, classId, features))
-            
-            if self.exampleCallback != None:
-                self.exampleCallback(example=example, cls=classId, features=features, **self.exampleCallbackArgs)
+            if exampleWriter != None:
+                exampleWriter.writeExample(classId, features)
         
         self.saveMetaData()
     
     def getFingerprint(self):
-        return 
+        return inspect.getsource(self.__class__)
     
     def saveMetaData(self):
         if self.metaDataFileName != None:
@@ -159,14 +161,15 @@ class Experiment:
             json.dump(output, f, indent=4)#, separators=(',\n', ':'))
             f.close()
         
-    def writeExample(self, example, classId, features):
-        self.writer()
+#     def writeExample(self, example, classId, features):
+#         self.writer()
+#     
+#     def writeExamples(self, exampleIO, metaFilePath): 
+#         con = self.getConnection()
+#         writerArgs, opened = example.openOutputFiles(featureFilePath, labelFilePath, writer)
+#         experimentMeta = {"X":featureFilePath, "y":labelFilePath, "writer":self.writer.__name__}
+#         if "fY" not in writerArgs:
+#             del experimentMeta["y"]
+#         featureIds = getExamples(con, experimentName, writer, writerArgs, metaFilePath, experimentOptions, experimentMeta)
+#         example.closeOutputFiles(opened, writer, featureFilePath, len(featureIds))
     
-    def writeExamples(self, ): 
-        con = self.getConnection()
-        writerArgs, opened = example.openOutputFiles(featureFilePath, labelFilePath, writer)
-        experimentMeta = {"X":featureFilePath,"y":labelFilePath,"writer":writer.__name__}
-        if "fY" not in writerArgs:
-            del experimentMeta["y"]
-        featureIds = getExamples(con, experimentName, writer, writerArgs, metaFilePath, experimentOptions, experimentMeta)
-        example.closeOutputFiles(opened, writer, featureFilePath, len(featureIds))
--- experiments.py
+++ experiments.py
@@ -1,9 +1,15 @@
+import sys, os
 from data.project import Experiment
+import data.writer
 
-SSM_GENE_CONSEQUENCE = "SELECT ('SSM:'||gene_affected||':'||consequence_type),1 FROM simple_somatic_mutation_open WHERE icgc_specimen_id={example['icgc_specimen_id']}"
+DATA_PATH = os.path.expanduser("~/data/CAMDA2015-data-local/")
+DB_PATH = os.path.join(DATA_PATH, "database/ICGC-18-150514.sqlite")
 
-def RemissionMutTest(Experiment):
+SSM_GENE_CONSEQUENCE = "SELECT ('SSM:'||gene_affected||':'||consequence_type),1 FROM simple_somatic_mutation_open WHERE icgc_specimen_id=?"
+
+class RemissionMutTest(Experiment):
     def __init__(self):
+        Experiment.__init__(self)
         self.project = "KIRC-US"
         self.exampleTable = "clinical"
         self.exampleFields = "icgc_donor_id,icgc_specimen_id,project_code,donor_vital_status,disease_status_last_followup,specimen_type,donor_interval_of_last_followup"
@@ -16,3 +22,17 @@ def RemissionMutTest(Experiment):
             """
         self.featureGroups = [SSM_GENE_CONSEQUENCE]
         self.filter = "SELECT * FROM simple_somatic_mutation_open WHERE icgc_specimen_id=? LIMIT 1"
+
+if __name__ == "__main__":
+    import argparse
+    parser = argparse.ArgumentParser(description='Run University of Turku experiments for CAMDA 2014')
+    parser.add_argument('-o','--output', help='Output directory', default=None)
+    parser.add_argument('-b','--icgcDB', default=DB_PATH, dest="icgcDB")
+    options = parser.parse_args()
+    
+    e = RemissionMutTest()
+    e.databasePath = options.icgcDB
+    exampleIO = data.writer.SVMLightExampleIO(os.path.join(options.output, "examples"))
+    exampleIO.newFiles()
+    e.buildExamples(os.path.join(options.output, "meta.json"), exampleIO)
+    exampleIO.closeFiles()
