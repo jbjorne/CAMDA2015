266fa76a1cdfbbca69d7b8033fb705257fff077d
jari <jari.bjorne@utu.fi>
2015-11-26 18:59:17 +0200

    Improved the command line interface.

--- experiments.py
+++ experiments.py
@@ -2,6 +2,14 @@ import os
 from data.project import Experiment
 from learn.Classification import Classification
 from learn.FeatureGroup import FeatureGroup
+import utils.Stream as Stream
+
+def splitOptions(optionString, allowedValues=None, delimiter=","):
+    actions = [x.strip() for x in optionString.split(delimiter)]
+    if allowedValues:
+        for action in actions:
+            assert action in allowedValues
+    return actions
 
 DATA_PATH = os.path.expanduser("~/data/CAMDA2015-data-local/")
 DB_PATH = os.path.join(DATA_PATH, "database/ICGC-18-150514.sqlite")
@@ -53,14 +61,15 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Run University of Turku experiments for CAMDA 2015')
     parser.add_argument('-o', '--output', help='Output directory', default=None)
     parser.add_argument('-d', "--debug", default=False, action="store_true", dest="debug")
-    groupE = parser.add_argument_group('Examples', 'Example Generation')
-    groupE.add_argument('-e', "--examples", default=False, action="store_true", dest="examples")
-    groupE.add_argument('-x', '--experiment', help='Output directory', default="RemissionMutTest")
+    parser.add_argument('-a', "--action", default=None, dest="action")
+    groupE = parser.add_argument_group('build', 'Example Generation')
+    #groupE.add_argument('-e', "--examples", default=False, action="store_true", dest="examples")
+    groupE.add_argument('-e', '--experiment', help='', default="RemissionMutTest")
     groupE.add_argument('--projects', help='Projects used in example generation', default=None)
     groupE.add_argument('-b', '--icgcDB', default=DB_PATH, dest="icgcDB")
-    groupC = parser.add_argument_group('Classification', 'Example Classification')
+    groupC = parser.add_argument_group('classify', 'Example Classification')
     groupC.add_argument('-c','--classifier', help='', default=None)
-    groupC.add_argument('-a','--classifierArguments', help='', default=None)
+    groupC.add_argument('-r','--classifierArguments', help='', default=None)
     groupC.add_argument('-m','--metric', help='', default="roc_auc")
     groupC.add_argument('-i','--iteratorCV', help='', default='getStratifiedKFoldCV')
     groupC.add_argument('-f','--numFolds', help='Number of folds in cross-validation', type=int, default=10)
@@ -68,11 +77,16 @@ if __name__ == "__main__":
     groupC.add_argument('-p', '--parallel', help='Cross-validation parallel jobs', type=int, default=1)
     groupC.add_argument("--hidden", default=False, action="store_true", dest="hidden")
     groupC.add_argument('--preDispatch', help='', default='2*n_jobs')
-    groupA = parser.add_argument_group('Analysis', 'Analysis for classified data')
-    groupA.add_argument("--cosmic", default=False, action="store_true", dest="cosmic")
+    #groupA = parser.add_argument_group('Analysis', 'Analysis for classified data')
+    #groupA.add_argument("--cosmic", default=False, action="store_true", dest="cosmic")
     options = parser.parse_args()
     
-    if options.examples:
+    actions = splitOptions(options.action, ["build", "classify", "analyse"])
+    
+    Stream.openLog(os.path.join(options.output, "log.txt"), clear = False)
+    print "Options:", options.__dict__
+    
+    if "build" in actions:
         print "======================================================"
         print "Building Examples"
         print "======================================================"
@@ -85,7 +99,8 @@ if __name__ == "__main__":
         e.writeExamples(options.output)
     
     resultPath = os.path.join(options.output, "classification.json")
-    if options.classifier != None:
+    classification = None
+    if "classify" in actions:
         print "======================================================"
         print "Classifying"
         print "======================================================"
@@ -96,13 +111,13 @@ if __name__ == "__main__":
         classification.readExamples(options.output)
         classification.classify(resultPath)
     
-    if options.cosmic:
+    if "analyse" in actions:
         print "======================================================"
         print "Analysing"
         print "======================================================"
         from learn.Analysis import COSMICAnalysis
         meta = resultPath
-        if options.classifier != None:
+        if classification != None:
             meta = classification.meta
         analysis = COSMICAnalysis(meta, dataPath=DATA_PATH)
         analysis.analyse(options.output, "cosmic")
