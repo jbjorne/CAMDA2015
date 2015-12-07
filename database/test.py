0bba193b67b66670866e22ad491184a352dc848a
jari <jari.bjorne@utu.fi>
2015-12-07 21:21:01 +0200

    More work on ICGC downloads.

--- database/test.py
+++ database/test.py
@@ -1,8 +1,10 @@
 import os
 import wget
 import requests
+from clint.textui import progress
 from urllib import urlopen
 import json
+import shutil
 
 dataTypes = {"ssm":"simple_somatic_mutation.open",
              "pexp":"protein_expression"}
@@ -12,16 +14,10 @@ downloadTemplate = "https://dcc.icgc.org/api/v1/download?fn=/release_20/Projects
 projectsURL = "https://dcc.icgc.org/api/v1/projects?size=100"
 
 
-def download(project, dataType, outDir):
-    if not os.path.exists(outDir):
-        os.makedirs(outDir)
-    else:
-        assert os.path.isdir(outDir)
+def download(project, dataType, outDir, clear=False):
     downloadURL = downloadTemplate.replace("PROJECT_CODE", project).replace("DATA_TYPE", dataTypes.get(dataType, dataType))
-    filename = os.path.join(outDir, os.path.basename(downloadURL))
-    if os.path.exists(filename):
-        os.remove(filename)
-    filename = wget.download(downloadURL, outDir)
+    downloadFile(downloadURL, outDir, clear=clear)
+    #filename = wget.download(downloadURL, outDir)
     
 def test(dataTypes):
     response = requests.get(projectsURL)
@@ -31,16 +27,43 @@ def test(dataTypes):
         if "ssm" in project.get("availableDataTypes"):
             download(project["id"], "ssm", "/tmp/download")
 
-def downloadProjects(downloadDir):
+def downloadFile(url, outDir, clear=False):
+    if not os.path.exists(outDir):
+        os.makedirs(outDir)
+    assert os.path.isdir(outDir)
+    filename = os.path.join(outDir, os.path.basename(url))
+    if os.path.exists(filename):
+        if clear:
+            os.remove(filename)
+        else:
+            print "Skipping", filename
+            return
+    print "Downloading", filename
+    r = requests.get(url, stream=True)
+    with open(filename, 'wb') as f:
+        total_length = int(r.headers.get('content-length'))
+        for chunk in progress.bar(r.iter_content(chunk_size=1024), expected_size=(total_length/1024) + 1): 
+            if chunk:
+                f.write(chunk)
+                f.flush()
+
+def downloadProjects(downloadDir, skipTypes, clear=False):
+    if clear and os.path.exists(downloadDir):
+        shutil.rmtree(downloadDir)
+    
     projects = requests.get(projectsURL)
     projects = projects.json()["hits"]
     basicData = ["donor", "sample", "specimen"]
+    count = 0
     for project in projects:
         projectId = project["id"]
+        print "Processing project", projectId, "(" + str(count) + "/" + str(len(projects)) + ")"
         availableDataTypes = project.get("availableDataTypes", [])
         for dataType in basicData + availableDataTypes:
+            print "  Downloading", dataType
             dataType = dataTypes.get(dataType, dataType)
-            download(project["id"], dataType, downloadDir)
+            download(projectId, dataType, downloadDir, clear=clear)
+        count += 1
     
     #print r.text[0:1000]
     #filename = wget.download(url2, "/tmp/download/projects.json")
@@ -53,4 +76,11 @@ def downloadProjects(downloadDir):
     #result = json.loads(url)
     
 if __name__ == "__main__":
-    test(["donor", "sample", "specimen", "ssm", "cnsm", "exp_seq", "exp_array"])
+    import argparse
+    parser = argparse.ArgumentParser(description='')
+    parser.add_argument('-o','--output', default=None, help="Output directory")
+    parser.add_argument('-s','--skipTypes', default="meth_array", help="Do not download these dataTypes")
+    parser.add_argument('-c','--clear', help='Delete existing downloads', action='store_true', default=False)
+    options = parser.parse_args()
+    
+    downloadProjects(options.output, options.skipTypes.split(","), options.clear)
