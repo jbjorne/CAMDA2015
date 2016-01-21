4b5ce1aacbd0a8a215f41beb2bb5d1b159a4211c
jari <jari.bjorne@utu.fi>
2016-01-21 23:18:56 +0200

    More testing.

--- learn/skext/BalancedIteratorCV.py
+++ learn/skext/BalancedIteratorCV.py
@@ -4,51 +4,43 @@ import numpy as np
 import random
 
 class BalancedIteratorCV():
-    def __init__(self, y_train, n_folds, shuffle, random_state, examples, groupBy):
+    def __init__(self, y_train, n_folds, shuffle, random_state, examples, groupBy, balanced=True):
         assert len(y_train) == len(examples)
         self.cv = StratifiedKFold(y_train, n_folds=n_folds, shuffle=shuffle, random_state=random_state)
         self.folds = [x for x in self.cv]
         
-        exampleByIndex = {i:examples[i] for i in range(len(examples))}
-        for foldIndex in range(len(self.folds)): 
-            train_indices, test_indices = self.folds[foldIndex]
-            # Count examples grouped
-            counts = defaultdict(lambda: defaultdict(int))
-            indices = {}
-            i = 0
-            for i in train_indices:
-                i = int(i)
-                example = exampleByIndex[i]
-                group = example[groupBy]
-                classId = y_train[i]
-                counts[group][classId] += 1
-                if group not in indices: indices[group] = {}
-                if classId not in indices[group]:
-                    indices[group][classId] = []
-                indices[group][classId].append(i)
-            groupSizes = {}
-            for groupKey in counts:
-                groupSizes[groupKey] = sum(counts[groupKey].values())
-            maxGroupSize = max(groupSizes.values())
-            # Determine per class sizes
-            extra = defaultdict(lambda: defaultdict(int))
-            for groupKey in counts:
-                groupCounts = counts[groupKey]
-                minorityClassSize = groupCounts[min(groupCounts, key=groupCounts.get)]
-                groupFactor = maxGroupSize / groupSizes[groupKey]
-                assert groupFactor >= 1.0
-                for classId in groupCounts:
-                    baseSize = groupCounts[classId]
-                    newSize = groupCounts[classId] + (groupCounts[classId] - minorityClassSize)
-                    newSize *= groupFactor
-                    extra[groupKey][classId] = newSize - baseSize
-            # Oversample training data
-            newIndices = []
-            for groupKey in extra:
-                for classId in extra[groupKey]:
-                    newIndices.extend([random.choice(indices[groupKey][classId]) for _ in range(extra[groupKey][classId])])
-                    #newIndices.extend(random.sample(indices[groupKey][classId], extra[groupKey][classId]))
-            self.folds[foldIndex] = (np.append(train_indices, np.array(newIndices)), test_indices)
+        if balanced:
+            exampleByIndex = {i:examples[i] for i in range(len(examples))}
+            for foldIndex in range(len(self.folds)): 
+                train_indices, test_indices = self.folds[foldIndex]
+                # Count examples grouped
+                counts = defaultdict(lambda: defaultdict(int))
+                indices = {}
+                i = 0
+                for i in train_indices:
+                    i = int(i)
+                    example = exampleByIndex[i]
+                    group = example[groupBy]
+                    classId = y_train[i]
+                    counts[group][classId] += 1
+                    if group not in indices: indices[group] = {}
+                    if classId not in indices[group]:
+                        indices[group][classId] = []
+                    indices[group][classId].append(i)
+                # Determine per class sizes
+                extra = defaultdict(lambda: defaultdict(int))
+                for groupKey in counts:
+                    groupCounts = counts[groupKey]
+                    minorityClassSize = groupCounts[min(groupCounts, key=groupCounts.get)]
+                    for classId in groupCounts:
+                        extra[groupKey][classId] = groupCounts[classId] - minorityClassSize
+                # Oversample training data
+                newIndices = []
+                for groupKey in extra:
+                    for classId in extra[groupKey]:
+                        newIndices.extend([random.choice(indices[groupKey][classId]) for _ in range(extra[groupKey][classId])])
+                        #newIndices.extend(random.sample(indices[groupKey][classId], extra[groupKey][classId]))
+                self.folds[foldIndex] = (np.append(train_indices, np.array(newIndices)), test_indices)
         
     def __len__(self):
         return len(self.cv)
